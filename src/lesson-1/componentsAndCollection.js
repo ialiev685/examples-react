9199d86d2f72b2e6a76c3840eef193e7bfa1457e
ilfat Aliev <ialiev685@gmail.com>
2021-07-25 01:21:33 +0300

    изучаю занятие 1

--- src/lesson-1/componentsAndCollection.js
+++ src/lesson-1/componentsAndCollection.js
@@ -32,6 +32,98 @@ const container = React.createElement(
   nextPost
 );
 
-ReactDOM.render(container, document.getElementById("root"));
+// ReactDOM.render(container, document.getElementById("root"));
 
 //-------Компоненты-------
+const pictureUrl = 'https://images.pexels.com/photos/8704785/pexels-photo-8704785.jpeg?cs=srgb&dl=pexels-daria-ponomareva-8704785.jpg&fm=jpg'
+
+const App =()=>(
+  <div>
+    <Product name ='На отдых в Турцию' text = 'Лететь всего 3.5 часа. Теплое море' imgUrl={pictureUrl}/>
+  </div>
+);
+
+
+const Product = props =>(
+<div>
+  <img src={props.imgUrl} width ='400'/>
+  <h2>{props.name}</h2>
+  <p>{props.text}</p>
+</div>
+);
+
+
+
+// ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+
+//-------props.children-------
+
+
+const Profile =({name, email})=>(
+  <div>
+      <p>name:{name}</p>
+      <p>email:{email}</p>
+  </div>
+);
+
+
+
+const Panel =({title, children})=>(
+  <div>
+    <h2>{title}</h2>
+      {children}
+  </div>
+);
+
+const App2=()=>(
+  <>
+  <Panel title='Пользователь:'>
+    <Profile name='Ilfat' email='ialiev685@gmail.com'/>
+  </Panel>
+  </>
+);
+
+// ReactDOM.render(<App2/>, document.getElementById("root"))
+
+//-------Свойство defaultProps-------
+
+
+
+
+
+
+const Item = ({imgUrl, name, text }) =>(
+<div>
+  <img src={imgUrl} width ='400'/>
+  <h2>{name}</h2>
+  <p>{text}</p>
+</div>
+);
+
+
+Item.defaultProps = {
+  imgUrl: 'https://images.pexels.com/photos/8829906/pexels-photo-8829906.jpeg?cs=srgb&dl=pexels-kira-schwarz-8829906.jpg&fm=jpg',
+};
+
+
+const App4 =()=>(
+  <div>
+    <Product name ='На отдых в Турцию' text = 'Лететь всего 3.5 часа. Теплое море' />
+  </div>
+);
+
+
+ReactDOM.render(<App4/>, document.getElementById("root"))
+
+
+//-------propTypes-------
+
+//npm install --save-dev prop-types
+
+// Product.propTypes = {
+//   imgUrl: PropTypes.string,
+//   name: PropTypes.string.isRequired,
+//   price: PropTypes.number.isRequired,
+// };
